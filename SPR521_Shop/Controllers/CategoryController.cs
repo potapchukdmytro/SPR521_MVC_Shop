4f1233c829833025e0696d6545ca421ad596b8c6
potapchukdmytro <traiget@gmail.com>
2026-08-22 10:38:23 +0300

    Add project files.

---
+++ SPR521_Shop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPR521_Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SPR521_Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SPR521_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
